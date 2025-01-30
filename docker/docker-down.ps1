ee5fec5a2f16e2b0e678dd3d7781a170ad39f59f
RobinSalihAxelsson <axelsson.robin@outlook.com>
2025-01-30 13:33:13 +0100

    fix: docker-down

--- docker/docker-down.ps1
+++ docker/docker-down.ps1
@@ -1,4 +1,5 @@
 #!/usr/bin/env pwsh
 
-$dockerCompose = Join-Path $PSScriptRoot docker-compose.infra.yml
-docker-compose -f $dockerCompose down
+$db = Join-Path $PSScriptRoot docker-compose.database.yml
+$proxy = Join-Path $PSScriptRoot docker-compose.proxy.yml
+docker-compose -f $db -f $proxy down
